7ccc456af00c1261b436f7c965d6221df728d7e5
Rob <rob.bast@gmail.com>
2025-08-06 15:13:56 +0000

    nog meer aanpassingen aan srcset script

--- srcset.sh
+++ srcset.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
+#
+# Example:
+#
+# for f in fotos/dir/*.jpg; do ./srcset.sh "$f" >> htmlmarkup.html; done
+#
+
 filename="$1"
 path="${filename%/*}"
 file_with_ext="${filename##*/}"
 file="${file_with_ext%.*}"
-prefix="${path}/resized/${file}"
-type=webp
 
 test ! -d "${path}/resized" && mkdir -p "${path}/resized"
 
-convert "$filename" -strip "${prefix}.${type}"
+# Convert the original image to webp format
+convert "$filename" -strip "${path}/resized/${file}.webp"
 
+# Create resized versions of the image
 for size in 320 640 768 1024 1366 1440 1600 1920; do
-  convert "$filename" -strip -adaptive-resize "$size" "${prefix}-${size}w.${type}"
+  convert "$filename" -strip -adaptive-resize "$size" "${path}/resized/${file}-${size}w.webp"
 done
 
+# Generate HTML markup for the image with srcset
 echo "<figure class=\"col-12 col-md-4\">"
-echo "  <a href=\"${prefix}.${type}\" data-pswp-srcset=\""
+echo "  <a href=\"resized/${file}.webp\" data-pswp-srcset=\""
 for size in 320 640 768 1024 1366 1440 1600 1920; do
-  echo "    ${prefix}-${size}w.${type} ${size}w,"
+  echo "    resized/${file}-${size}w.webp ${size}w,"
 done
-echo "  \" $(identify -format 'data-pswp-width=\"%w\" data-pswp-height=\"%h\"' ${prefix}.${type}) target=\"_blank\">"
-echo "    <img class=\"figure-img img-thumbnail\" src=\"${prefix}-768w.${type}\" />"
+echo "  \" $(identify -format 'data-pswp-width=\"%w\" data-pswp-height=\"%h\"' "${path}/resized/${file}.webp") target=\"_blank\">"
+echo "    <img class=\"figure-img img-thumbnail\" src=\"${path}/resized/${file}-768w.webp\" />"
 echo "  </a>"
 echo "  <figcaption>Lorem ipsum</figcaption>"
 echo "</figure>"
